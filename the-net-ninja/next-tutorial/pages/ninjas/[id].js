5ea1e1f16afe3dadb40171b097876389d683f934
joakimvu <joakim.vu@hiof.no>
2022-09-14 13:55:45 +0200

    Tutorial part 13/14
    * npm run build => Failed to compile.

--- the-net-ninja/next-tutorial/pages/ninjas/[id].js
+++ the-net-ninja/next-tutorial/pages/ninjas/[id].js
@@ -1,24 +1,44 @@
 // This runs before the components gets rendered
+// How many HTML pages need to be made from our data?
 export const getStaticPaths = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-  const paths = data?.map((ninja) => {
+  // map data to an array of path objects with params (id)
+  const paths = data.map((ninja) => {
     return {
-      params: { id: ninja.id.tostring() },
+      params: { id: ninja.id.toString() },
     };
   });
 
   return {
+    // next knows how many pages need to made from the number of paths
     paths: paths,
     fallback: false,
   };
 };
 
-const ninjaDetails = () => {
+// For each of the paths this runs and fetch each item we need
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users/" + id
+  );
+  const data = await response.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+// Putting data in here to generate a templete for each of the pages
+const ninjaDetails = ({ ninja }) => {
   return (
     <div>
-      <h1>Details pages</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      {/* <p>{ninja.address.city}</p> */}
     </div>
   );
 };

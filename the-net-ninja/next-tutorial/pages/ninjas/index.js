1ee8e911052ebc8d73b73f5e03e36adbe5bc0cde
joakimvu <joakim.vu@hiof.no>
2022-09-14 13:12:28 +0200

    Finished tutorial 10/14

--- the-net-ninja/next-tutorial/pages/ninjas/index.js
+++ the-net-ninja/next-tutorial/pages/ninjas/index.js
@@ -1,7 +1,27 @@
-const ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+// This runs before the components gets rendered
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const ninjas = ({ ninjas }) => {
+  console.log(ninjas);
   return (
     <div>
       <h1>All Ninjas Welcome!</h1>
+      {ninjas?.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };

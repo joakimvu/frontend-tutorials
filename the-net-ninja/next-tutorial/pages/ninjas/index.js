c5caa9bc9617c92da63c6a4260487de4ecda2817
joakimvu <joakim.vu@hiof.no>
2022-09-14 12:20:47 +0200

    Finished part 6/14

---
+++ the-net-ninja/next-tutorial/pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const ninjas = () => {
+  return (
+    <div>
+      <h1>All Ninjas Welcome!</h1>
+    </div>
+  );
+};
+
+export default ninjas;
---
+++ the-net-ninja/next-tutorial/pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const test = () => {
+  return (
+    <div>
+      <h1>Test Ninjas</h1>
+    </div>
+  );
+};
+
+export default test;

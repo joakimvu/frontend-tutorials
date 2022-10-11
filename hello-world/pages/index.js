61af4b25f486d5b35ae67b1449bd01a2ead0122c
joakimvu <joakim.vu@hiof.no>
2022-10-11 23:56:05 +0200

    Initial Commit

---
+++ hello-world/pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+// Tutorial from
+// https://www.youtube.com/watch?v=9P8mASSREYM&list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH&index=2
+export default function Home() {
+  return <h1>Home page</h1>;
+}

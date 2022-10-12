1dbe008768b7a2815c536fecc662192c5aed95e3
joakimvu <joakim.vu@hiof.no>
2022-10-12 15:20:39 +0200

    Starting tutorial part 26

---
+++ next-pre-rendering/pages/posts/[postId].js
@@ -0,0 +1,42 @@
+export default function Post({ post }) {
+  return (
+    <>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <p>{post.body}</p>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}

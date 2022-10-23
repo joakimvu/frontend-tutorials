b5f9431a36bfcf93d9905f121f336bcc692dd014
joakimvu <joakim.vu@hiof.no>
2022-10-23 16:50:00 +0200

    Finished tutorial part 47
    API - DELETE request

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -4,12 +4,14 @@ export default function CommentsPage() {
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState("");
 
+  // Fetching all comments from database
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
 
+  // Submitting a new comments to database
   const submitComment = async () => {
     const response = await fetch("/api/comments", {
       method: "Post",
@@ -20,6 +22,16 @@ export default function CommentsPage() {
     });
     const data = await response.json();
     console.log(data);
+  };
+
+  // Delete a comment
+  const deleteComment = async (commentId) => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchComments();
   };
 
   return (
@@ -35,6 +47,7 @@ export default function CommentsPage() {
         return (
           <div key={comment?.id}>
             {comment?.id} {comment?.text}
+            <button onClick={() => deleteComment(comment?.id)}>Delete</button>
           </div>
         );
       })}

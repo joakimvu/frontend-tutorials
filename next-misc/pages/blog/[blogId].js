d9484b3736e5b2d65234c374393c4c3a2f6f8ffc
joakimvu <joakim.vu@hiof.no>
2022-10-25 17:21:24 +0200

    Next.js Tutorial - 64
    Environment Variables

--- next-misc/pages/blog/[blogId].js
+++ next-misc/pages/blog/[blogId].js
@@ -7,23 +7,41 @@ export default function Blog({ title, description }) {
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-      <h1 className="content">Article</h1>
+      <h1 className="content">
+        ENV Anaytics {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      </h1>
     </>
   );
 }
 
-export async function getStaticPaths() {
-  return {
-    paths: [{ params: { blogId: "1" } }],
-    fallback: false,
-  };
-}
+export async function getServerSideProps() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
 
-export async function getStaticProps() {
+  console.log(
+    `Conneting to database with username ${user} and passowrd ${password}`
+  );
+
   return {
     props: {
-      title: "Article Ttile",
+      title: "Article Title",
       description: "Article description",
     },
   };
 }
+
+// export async function getStaticPaths() {
+//   return {
+//     paths: [{ params: { blogId: "1" } }],
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       title: "Article Ttile",
+//       description: "Article description",
+//     },
+//   };
+// }

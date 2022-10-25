3e860f07900a1d66a47f5fbfd186f7958c3a6ab9
joakimvu <joakim.vu@hiof.no>
2022-10-25 17:08:30 +0200

    Next.js Tutorial - 63
    Redirects

--- next-misc/next.config.js
+++ next-misc/next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/old-blog/:id",
+        destination: "/new-blog/:id",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

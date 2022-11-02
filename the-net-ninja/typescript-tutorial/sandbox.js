b7c608505a1699a2d77dc16ea31bd049697a2cf2
joakimvu <joakim.vu@hiof.no>
2022-11-02 16:08:53 +0100

    update

--- the-net-ninja/typescript-tutorial/sandbox.js
+++ the-net-ninja/typescript-tutorial/sandbox.js
@@ -1,6 +1,19 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
+};
+ninja.age = 40;
+ninja.name = "ryu";
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40
+};
--- the-net-ninja/typescript-tutorial/sandbox.ts
+++ the-net-ninja/typescript-tutorial/sandbox.ts
@@ -1,9 +1,27 @@
-const character = "mario";
+let names = ["luigi", "mario", "yoshi"];
 
-console.log(character);
+names.push("toad");
 
-const inputs = document.querySelectorAll("input");
+let numbers = [10, 20, 30, 40];
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+numbers.push(25);
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10);
+
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = "ryu";
+
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  age: 40,
+};

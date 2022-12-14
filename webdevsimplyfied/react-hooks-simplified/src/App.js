5d235d31f26ee63c22a791400880499f50cf416d
Joakim Vu <joakimvu@icloud.com>
2022-12-14 19:34:27 +0100

    useRef

--- webdevsimplyfied/react-hooks-simplified/src/App.js
+++ webdevsimplyfied/react-hooks-simplified/src/App.js
@@ -1,3 +1,44 @@
+import React, { useState, useMemo, useEffect } from "react";
+
 export default function App() {
-  return <>a</>;
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  // const themeStyles = useMemo(() => {
+  //   return {
+  //     backgroundColor: dark ? "black" : "white",
+  //     color: dark ? "white" : "black",
+  //   };
+  // }, [dark]);
+  const themeStyles = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
+  // useEffect(() => {
+  //   console.log("Theme Changed");
+  // }, [themeStyles]);
+
+  return (
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(num) {
+  console.log("Calling Slow Function");
+  for (let i = 0; i <= 100000000; i++) {
+    return num * 2;
+  }
 }
